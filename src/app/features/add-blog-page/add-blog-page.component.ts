877ae448b66b437a072cc2368a63a3fbed6545bf
Felice Pettinaro <felice.pettinaro@gmail.com>
2023-08-25 11:04:47 +0200

    feat: form and validator added

--- src/app/features/add-blog-page/add-blog-page.component.ts
+++ src/app/features/add-blog-page/add-blog-page.component.ts
@@ -1,8 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {
+  FormControl,
+  FormGroup,
+  UntypedFormControl,
+  UntypedFormGroup,
+  Validators,
+} from '@angular/forms';
+
+type InvalidFormGroup = {
+  title: FormControl<string | null>;
+  content: FormControl<string | null>;
+};
 
 @Component({
   selector: 'app-add-blog-page',
   templateUrl: './add-blog-page.component.html',
   styleUrls: ['./add-blog-page.component.scss'],
 })
-export class AddBlogPageComponent {}
+export class AddBlogPageComponent implements OnInit {
+  form!: FormGroup<InvalidFormGroup>;
+
+  ngOnInit() {
+    this.form = new FormGroup<InvalidFormGroup>({
+      title: new FormControl<string | null>('an existing title', [
+        Validators.required,
+        Validators.maxLength(10),
+      ]),
+      content: new FormControl<string | null>(null),
+    });
+  }
+
+  onSubmit() {
+    console.log(this.form);
+  }
+}
