b896d5f6cb491fc2d57a23d565ffe0494811ec25
Felice Pettinaro <felice.pettinaro@gmail.com>
2023-08-25 13:45:36 +0200

    fix: verschiedene anpassungen vorgenomen um form und validation

--- src/app/features/add-blog-page/add-blog-page.component.ts
+++ src/app/features/add-blog-page/add-blog-page.component.ts
@@ -4,12 +4,19 @@ import {
   FormGroup,
   UntypedFormControl,
   UntypedFormGroup,
+  ValidationErrors,
   Validators,
 } from '@angular/forms';
+import { BlogStateService } from './state/blog-state.service';
 
 type InvalidFormGroup = {
   title: FormControl<string | null>;
   content: FormControl<string | null>;
+};
+
+type CreatdeBlog = {
+  title: string;
+  content: string;
 };
 
 @Component({
@@ -20,17 +27,31 @@ type InvalidFormGroup = {
 export class AddBlogPageComponent implements OnInit {
   form!: FormGroup<InvalidFormGroup>;
 
+  constructor(public blogStateService: BlogStateService) {}
+
   ngOnInit() {
     this.form = new FormGroup<InvalidFormGroup>({
       title: new FormControl<string | null>('an existing title', [
         Validators.required,
         Validators.maxLength(10),
+        customeValidator,
       ]),
       content: new FormControl<string | null>(null),
     });
   }
 
   onSubmit() {
-    console.log(this.form);
+    if (this.form.valid) {
+      this.blogStateService.addBlog(this.form.value as CreatdeBlog);
+    }
   }
 }
+
+const customeValidator = (
+  control: UntypedFormControl
+): ValidationErrors | null => {
+  if (control.value === 'Felice') {
+    return { NotUseNameFromDev: true };
+  }
+  return null;
+};
